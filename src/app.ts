9065ffbe16a7b0267af5c3666ae1de6e605710c3
Robert Lowe <robertmlowe@rmlowe.com>
2020-05-10 10:25:47 +0800

    Rest Parameters

--- src/app.ts
+++ src/app.ts
@@ -18,17 +18,17 @@ age = 29;
 
 // console.log(result);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-  button.addEventListener('click', event => console.log(event));
-}
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
@@ -41,3 +41,12 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
