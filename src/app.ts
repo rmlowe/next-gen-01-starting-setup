883c3f7dc30a98055a9f2df6f88e7ee19dfcfbd9
Robert Lowe <robertmlowe@rmlowe.com>
2020-05-10 09:33:30 +0800

    Arrow Functions

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
 // if (age > 20) {
 //   let isOld = true;
@@ -17,3 +17,15 @@ function add(a: number, b: number) {
 // console.log(isOld);
 
 // console.log(result);
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2));
